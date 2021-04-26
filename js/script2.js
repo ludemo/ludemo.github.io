f0fa6fa5aa33efc29c3f7004678d64d278f26cb6
Sofia2506 <sonque@unsa.edu.pe>
2021-04-26 00:11:55 -0500

    probando logros

--- js/script2.js
+++ js/script2.js
@@ -3,14 +3,12 @@ new Glide(".images",{
 	perView: 3,
 	gap: 40,
 	autoplay: 2500,
-	focusAt: 'center',
 	breakpoints: {
 		800:{
 			perView: 2,
 			gap: 40,
 		},
 		430:{
-			focusAt: 0,
 			perView: 1,
 			gap: 10,
 		}
