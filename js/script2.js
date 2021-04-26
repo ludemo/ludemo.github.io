3a683a3c4691fbdba53e8cca974eaa019b7a05d9
Sofia2506 <sonque@unsa.edu.pe>
2021-04-25 23:35:26 -0500

    Weno a ver

--- js/script2.js
+++ js/script2.js
@@ -5,12 +5,14 @@ new Glide(".images",{
 	autoplay: 2500,
 	focusAt: 'center',
 	breakpoints: {
-		1200:{
-			perView: 3
+		800:{
+			perView: 2,
+			gap: 40,
 		},
 		430:{
 			focusAt: 0,
 			perView: 1,
+			gap: 10,
 		}
 	}
 }).mount();
