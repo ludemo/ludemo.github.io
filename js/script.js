76f43d0989111d328348b93118659c406d53ceaf
zevallos-rgb <azevallosa@unsa.edu.pe>
2021-06-18 21:51:46 -0500

    separación de divs a modo vertical en tamaños moviles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-new WholePageSlider();
+
 let servicio = document.getElementsByClassName("servicio");
+let bloque2 = document.getElementById("b2");
 const media = matchMedia('(max-width:700px)');
 console.log("entro")
 console.log(media.matches)
 if(media.matches){
+    bloque2.classList.toggle("section");
+    bloque2.classList.toggle("bloque2");
     for(let i = 0 ;i < servicio.length ; i++){
-        servicio[i].className += " page";
+        servicio[i].className += " section ";
     }
 }
+new WholePageSlider({
+    containerId:'wholepage',
+    sectionClass: 'section',
+    pageClass: 'page'
+})
 //----------------
 let muestra= getComputedStyle(document.querySelector('.bloque1')).getPropertyValue('transform')
 console.log(muestra)
