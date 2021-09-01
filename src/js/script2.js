fec1a2c2c5c5f34371ff94a857af92b1acb2a6b9
zevallos-rgb <azevallosa@unsa.edu.pe>
2021-09-01 12:31:05 -0500

    responsive mejorado para el footer en un 80%

--- src/js/script2.js
+++ src/js/script2.js
@@ -5,14 +5,6 @@ new Glide(".images",{
 	autoplay: 2500,
 	focusAt: 'center',
 	breakpoints: {
-		1200:{
-			perView: 2,
-			gap: 40,
-		},
-		800:{
-			perView: 2,
-			gap: 40,
-		},
 		560:{
 			focusAt: 0,
 			perView: 1,
