b96c55293f2d1d11e5f011f1a130b35f413b94c6
Sofia2506 <sonque@unsa.edu.pe>
2023-03-31 13:49:43 -0500

    cambios que hay en mi

--- js/scripts/envio.js
+++ js/scripts/envio.js
@@ -1,5 +1,5 @@
 //Envio a google sheet
-let urlScriptSheet = "https://script.google.com/macros/s/AKfycbyZ7_QUqjMvl9JutDwSRfV3Y97DtYKPuLrUbDCR7xlzhxW4hi4VjYa0o7MHuoPKkJuZDg/exec";
+let urlScriptSheet = "https://script.google.com/macros/s/AKfycbxItl5PVwJruFbAy-boxtEt5C0ZqIbELkYBJDnGBX0DdjKpPHqvwfboxiTjng90yWbe/exec";
 //Envio para hacer pruebas y no gastar solicitudes :" 
 //let urlScriptSheet : "https://script.google.com/macros/s/AKfycbxItl5PVwJruFbAy-boxtEt5C0ZqIbELkYBJDnGBX0DdjKpPHqvwfboxiTjng90yWbe/exec"
 //Envio hacia whatsapp
@@ -88,7 +88,7 @@ formulario.addEventListener('submit', async (event) => {
         //Envio de datos sin imagen a google sheet
         try{
             //Link para pruebas :https://sheet.best/api/sheets/ccbce7ee-c532-4a77-8a92-233ea4dc8671
-            await fetch('https://sheet.best/api/sheets/6d93adc1-1678-4695-abc5-dcd0fc459ce1',{
+            await fetch('https://sheet.best/api/sheets/ccbce7ee-c532-4a77-8a92-233ea4dc8671',{
                 method : 'POST',
                 headers : {
                 'Content-Type' : 'application/json'
